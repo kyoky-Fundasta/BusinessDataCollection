5081294be24d78e8d66125aa7348f752ca1a46b3
kyoky-Fundasta <kyokyoku-an@fundasta.co.jp>
2024-08-08 20:17:43 +0900

    get title

--- module/crawl.py
+++ module/crawl.py
@@ -3,9 +3,23 @@ from data.const import start_url_jp
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get(start_url_jp)
 
-if response.status_code == 200:
-    page_content = response.content
-else:
-    print(f"Failed to retrive the page. Status code : {response.status_code}")
+def fetch_page(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        page_contents = BeautifulSoup(response.content, "html.parser")
+        return page_contents
+    else:
+        print(f"Failed to retrive the page. Status code : {response.status_code}")
+        return None
+
+
+def get_title(soup):
+    title = soup.find("h1", {"id": "firstHeading"}).get_text()
+    print(title)
+    return title
+
+
+def get_article(soup):
+    pass
